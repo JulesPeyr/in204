586b9ba635aa26267eee3bd18794b0af99f0dac7
Jules Peyrache <jules.peyrache@gmail.com>
2024-03-12 19:57:54 +0000

    ajouté objects.h, qui contient la sphère et la hitbox des futures formes. le main permet de tester l'ajout de plusieurs objets et le calcul des normales
     Changes to be committed:
    	modified:   hitable.h
    	modified:   main.cpp
    	new file:   objects.h

--- hitable.h
+++ hitable.h
@@ -1,10 +1,35 @@
 #include "ray.h"
 
-bool hit_sphere(const vector center, float radius, const ray& r){ /* définit la hitbox d'une sphère et si le rayon r la percute*/
-    vector oc = r.origin()-center;
-    float a = dot(r.direction(),r.direction());
-    float b = 2.0*dot(oc,r.direction());
-    float c = dot(oc,oc) - radius*radius;
-    float discriminant = b*b - 4*a*c;
-    return (discriminant>0);
+struct hit_record {
+    float t; 
+    vector p;
+    vector normal;
+};
+
+class hitable {  /*classe parente des objects avec lesquels les rayons pourront interagir*/
+    public:
+    virtual bool hit(const ray& r, float t_min,float t_max, hit_record& rec) const = 0;
+}; /* t_min et t_max sont les bornes de l'intervalle d'une interaction valide, pour éviter par exemple qu'un rayon se retrouve bloqué à l'infini entre deux surfaces parfaitement réfléchissantes*/
+
+class hitable_list: public hitable {    /*liste des collisions de notre rayons avec des objets*/
+    public:
+        hitable_list() {}
+        hitable_list(hitable** l, int n) {list = l; list_size = n;}
+        virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
+        hitable** list;
+        int list_size;
+};
+
+bool hitable_list::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+    hit_record temp_rec;
+    bool hit_anything = false;
+    double closest_so_far = t_max;
+    for (int i = 0; i < list_size; i++) {
+        if(list[i]->hit(r,t_min,closest_so_far, temp_rec)){
+            hit_anything = true;
+            closest_so_far = temp_rec.t;
+            rec = temp_rec;
+        }
+    }
+    return hit_anything;
 }
--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include "sdltemplate.h"    /* biblitothèque qui va nous permettre de "dessiner" sur un écran, template qui facilite son utilisation*/
+#include "objects.h"
+#include "float.h"
 
-#include "hitable.h"
 using namespace sdltemplate;
 
 
 
-vector color(const ray& r){
-    if(hit_sphere(vector(0,0,-1),0.5,r)){ /* si le rayon touche la sphère c'est vert*/
-        return vector(0,1,0);
+vector color(const ray& r, hitable* world){
+    hit_record rec; 
+    if(world->hit(r,0.0,MAXFLOAT,rec)){ /* si le rayon touche la sphère c'est un maxi dégradé en fonction de la normale*/
+        return 0.5*vector(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
     }
     vector unit_direction = unit_vector(r.direction());
     float t = 0.5*(unit_direction.y() + 1.0);  /*sinon le dégradé de bleu*/
@@ -24,12 +26,16 @@ int main(){
     vector horizontal(4.0,0.0,0.0);
     vector vertical(0.0,2.0,0.0);
     vector origin(0.0,0.0,0.0);
+    hitable* list[2];                           /*on a donc sur l'image deux boules*/
+    list[0] = new sphere(vector(0,0,-1),0.5);    /* petite boule*/
+    list[1] = new sphere(vector(0,-100.5,-1),100);  /* grosse boule*/
+    hitable* world = new hitable_list(list,2);
     for(int y = height-1; y>=00; y--) {
         for(int x=0; x<width; x++){
             float u = float(x) / float(width);
             float v = float(y) / float(height);
             ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            vector col = color(r);
+            vector col = color(r,world);
             int ir = int(255.99*col.r());
             int ig = int(255.99*col.g());
             int ib = int(255.99*col.b());
